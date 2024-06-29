6466af24c4165b643bd14be7967cdb13869b6fee
Andrew Bates <a@sprock.io>
2024-06-29 15:27:34 -0500

    Add reverse mode

--- main.go
+++ main.go
@@ -24,17 +24,20 @@ var (
 type Flags struct {
 	showHelp    bool
 	showVersion bool
+	reverseMode bool
+	uppercase   bool
 }
 
-func init() {
+func main() {
 
 	versionInfo := fmt.Sprintf("%s %s", version, commitID)
 	// Initialize flags
 	flag.BoolVar(&flags.showHelp, "help", false, "Display help information")
+	flag.BoolVar(&flags.reverseMode, "reverse", false, "Reverse mode: replace tokens with hashes")
+	flag.BoolVar(&flags.uppercase, "uppercase", false, "Uppercase hexadecimal numbers")
 	flag.BoolVar(&flags.showVersion, "version", false, fmt.Sprintf("Display version information: %s", versionInfo))
 	flag.Parse()
-}
-func main() {
+
 	if flags.showHelp {
 		fmt.Println("Usage: evrcat [OPTION]... [FILE]...")
 		fmt.Println("\nConcatenate FILE(s) to standard output, replacing hexadecimal numbers with tokens.")
@@ -65,7 +68,7 @@ func main() {
 
 			file, err := os.Open(file)
 			if err != nil {
-				fmt.Fprintln(os.Stderr, "Error opening file:", err)
+				fmt.Fprintf(os.Stderr, "Error opening file `%s`: %s", file.Name(), err.Error())
 				continue
 			}
 			defer file.Close()
@@ -74,17 +77,15 @@ func main() {
 
 		for scanner.Scan() {
 			line := scanner.Text()
-			matches := symbolPattern.FindAllString(line, -1)
-			replacements := make([]string, 0, len(matches)*2)
-			for _, match := range matches {
-				replacement := evr.ToSymbol(match).Token().String()
-				if !strings.HasPrefix(replacement, "0x") {
-					replacements = append(replacements, match, replacement)
-				}
-
+			if flags.reverseMode {
+				line = replaceTokens(line, flags.uppercase)
+			} else {
+				line = replaceSymbols(line)
 			}
-			line = strings.NewReplacer(replacements...).Replace(line)
-			fmt.Println(line)
+			// print out the line
+			if _, err := fmt.Println(line); err != nil {
+				fmt.Fprintln(os.Stderr, "Error writing to stdout:", err)
+			}
 		}
 		if err := scanner.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "reading file:", err)
@@ -92,15 +93,34 @@ func main() {
 	}
 }
 
+// replaceSymbols replaces all (known) symbol hashes in a line with their token representation.
 func replaceSymbols(line string) string {
 	matches := symbolPattern.FindAllString(line, -1)
 	replacements := make([]string, 0, len(matches)*2)
 	for _, match := range matches {
 		replacement := evr.ToSymbol(match).Token().String()
-		if strings.HasPrefix(replacement, "0x") {
-			replacement = strings.ToUpper(replacement)
+		if !strings.HasPrefix(replacement, "0x") {
+			replacements = append(replacements, match, replacement)
 		}
-		replacements = append(replacements, match, replacement)
 	}
-	return strings.NewReplacer(replacements...).Replace(line)
+	line = strings.NewReplacer(replacements...).Replace(line)
+	return line
+}
+
+// replaceTokens replaces all tokens in a line with their hashed representation.
+func replaceTokens(line string, uppercase bool) string {
+	tokens := strings.Split(line, " ")
+	hashes := make([]string, 0, len(tokens))
+	for _, t := range tokens {
+		if len(t) == 0 {
+			continue
+		}
+		sym := evr.ToSymbol(t)
+		s := sym.HexString()
+		if uppercase {
+			s = strings.ToUpper(s)
+		}
+		hashes = append(hashes, s)
+	}
+	return strings.Join(hashes, " ")
 }
